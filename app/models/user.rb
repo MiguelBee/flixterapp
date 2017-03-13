740900555d2397ad7f1b42da7df9630594279676
Miguel Bustamante <phatbastard81@yahoo.com>
2017-03-13 20:19:51 +0000

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :courses
+  
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
