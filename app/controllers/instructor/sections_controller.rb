e04c23299a0d6881af5baef8dc8ee9242a94cf61
Miguel Bustamante <phatbastard81@yahoo.com>
2017-03-29 16:38:58 +0000

    added drag and drop funcitonality to sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,20 +1,43 @@
 class Instructor::SectionsController < ApplicationController
     before_action :authenticate_user!
-    before_action :require_authorize_for_current_course
+    before_action :require_authorize_for_current_course, only: [:new, :create]
+    before_action :require_authorize_for_current_section, only: [:update]
     
     
     def new
-       
         @section = Section.new
     end
     
     def create
         @section = current_course.sections.create(section_params)
         redirect_to instructor_course_path(current_course)
-        
     end
     
+    def update
+        current_course.sections.update_attributes(section_params)
+        render text: "updated!"
+    end
+    
+    
 private
+
+helper_method :current_course
+helper_method :current_section
+
+def current_course
+    if params[:course_id]
+    @current_course ||= Course.find(params[:course_id])
+    else
+    current_section.course
+    end
+    
+end
+
+def require_authorize_for_current_section
+    if current_section.course.user != current_user
+        render text: 'Unauthorized', status: :unauthorized
+    end
+end
 
 def require_authorize_for_current_course
     if current_course.user != current_user
@@ -22,14 +45,13 @@ def require_authorize_for_current_course
     end
 end
 
-helper_method :current_course
-
-def current_course
-    @current_course ||= Course.find(params[:course_id])
-end
 
 def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
+end
+
+def current_section
+    @current_section ||= Section.find(params[:id])
 end
 
 end
